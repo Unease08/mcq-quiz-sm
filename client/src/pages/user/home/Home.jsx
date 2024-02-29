4eab5dc1341e23c08c0c5da5e84ac28631b95774
Anish Shrestha <anish08shrestha@gmail.com>
2024-02-29 16:49:49 +0545

    changes in the user interface

--- client/src/pages/user/home/Home.jsx
+++ client/src/pages/user/home/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="curves">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 255">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
           <path
-            fill="#6c63ff"
-            fillOpacity="0.29"
-            d="M0,256L48,234.7C96,213,192,171,288,128C384,85,480,43,576,74.7C672,107,768,213,864,234.7C960,256,1056,192,1152,170.7C1248,149,1344,171,1392,181.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            fill="#6C63FF"
+            fill-opacity="0.32"
+            d="M0,96L40,122.7C80,149,160,203,240,186.7C320,171,400,85,480,58.7C560,32,640,64,720,96C800,128,880,160,960,170.7C1040,181,1120,171,1200,165.3C1280,160,1360,160,1400,160L1440,160L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
           ></path>
         </svg>
       </div>
@@ -38,7 +38,8 @@ const Home = () => {
         </div>
       </div>
       <div className="subjects">
-        <div className="box box1">
+        <div className="box box1"></div>
+        <div className="box box2">
           <div className="waves">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path
@@ -50,23 +51,6 @@ const Home = () => {
           </div>
           <div className="box-title">
             <span>English</span>
-            <button className="submit" type="submit">
-              Start Now
-            </button>
-          </div>
-        </div>
-        <div className="box box2">
-          <div className="waves">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#fca7d6"
-                fill-opacity="1"
-                d="M0,320L26.7,298.7C53.3,277,107,235,160,197.3C213.3,160,267,128,320,149.3C373.3,171,427,245,480,261.3C533.3,277,587,235,640,186.7C693.3,139,747,85,800,90.7C853.3,96,907,160,960,202.7C1013.3,245,1067,267,1120,261.3C1173.3,256,1227,224,1280,202.7C1333.3,181,1387,171,1413,165.3L1440,160L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"
-              ></path>
-            </svg>
-          </div>
-          <div className="box-title">
-            <span>Math</span>
             <button className="submit" type="submit">
               Start Now
             </button>
@@ -89,7 +73,7 @@ const Home = () => {
             </button>
           </div>
         </div>
-        <div className="box box4">
+        {/* <div className="box box4">
           <div className="waves">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path
@@ -122,7 +106,7 @@ const Home = () => {
               Start Now
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="user-footer">
         <span>Powered by : </span>
