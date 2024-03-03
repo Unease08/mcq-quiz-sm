dc67604fc69f7275844b2cc2901d30272b8871aa
Anish Shrestha <anish08shrestha@gmail.com>
2024-03-03 14:35:26 +0545

    designing box1 for exam schedule

--- client/src/pages/user/home/Home.jsx
+++ client/src/pages/user/home/Home.jsx
@@ -38,7 +38,32 @@ const Home = () => {
         </div>
       </div>
       <div className="subjects">
-        <div className="box box1"></div>
+        <div className="box box1">
+          <span className="exam-schedule">Exam schedule</span>
+
+          <div className="schedule">
+            <div className="schedule-subject">
+              <span className="schedule-subject-heading">Subject</span>
+              <div className="schedule-subject-title-container">
+                <span className="schedule-subject-title">Math</span>
+                <span className="schedule-subject-title">Biology</span>
+                <span className="schedule-subject-title">Nepali</span>
+                <span className="schedule-subject-title">Computer</span>
+                <span className="schedule-subject-title">Physics</span>
+              </div>
+            </div>
+            <div className="schedule-date">
+              <span className="schedule-subject-heading">Date</span>
+              <div className="schedule-subject-title-container">
+                <span className="schedule-subject-title">2080-12-21</span>
+                <span className="schedule-subject-title">2080-12-21</span>
+                <span className="schedule-subject-title">2080-12-21</span>
+                <span className="schedule-subject-title">2080-12-21</span>
+                <span className="schedule-subject-title">2080-12-21</span>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="box box2">
           <div className="waves">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -50,29 +75,13 @@ const Home = () => {
             </svg>
           </div>
           <div className="box-title">
-            <span>English</span>
+            <span className="box-title-heading">English</span>
             <button className="submit" type="submit">
               Start Now
             </button>
           </div>
         </div>
-        <div className="box box3">
-          <div className="waves">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#f7ca98"
-                fill-opacity="1"
-                d="M0,320L26.7,298.7C53.3,277,107,235,160,197.3C213.3,160,267,128,320,149.3C373.3,171,427,245,480,261.3C533.3,277,587,235,640,186.7C693.3,139,747,85,800,90.7C853.3,96,907,160,960,202.7C1013.3,245,1067,267,1120,261.3C1173.3,256,1227,224,1280,202.7C1333.3,181,1387,171,1413,165.3L1440,160L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"
-              ></path>
-            </svg>
-          </div>
-          <div className="box-title">
-            <span>Physics</span>
-            <button className="submit" type="submit">
-              Start Now
-            </button>
-          </div>
-        </div>
+        <div className="box box3"></div>
         {/* <div className="box box4">
           <div className="waves">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
